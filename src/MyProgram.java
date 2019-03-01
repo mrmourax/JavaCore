e5c70b0612e62685075119bbe0e4cd9a9f1be2e1
mrmourax <mrmourax@gmail.com>
2019-03-01 22:49:18 +0200

    Scanner example 2

--- src/MyProgram.java
+++ src/MyProgram.java
@@ -1,10 +1,15 @@
+import javax.sound.midi.Soundbank;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MyProgram {
-    public static void main(String[] args){
+    public static void main(String[] args) throws AWTException {
         // Scanner class example
+        /*
         String stop = "";
         System.out.println("Welcome to AnimalHospital");
         Scanner in = new Scanner(System.in);
@@ -15,8 +20,8 @@ public class MyProgram {
             System.out.print("Input breed: ");
             String catBreed = in.nextLine();
 
-            System.out.println("Enter any key continue...");
-            System.out.println("Enter 0 to exit...");
+            System.out.println("Enter any key continue and press enter...");
+            System.out.println("Enter 0 to exit and press enter...");
 
             if(!in.hasNextLine()){
                 System.out.println("Sorry, this is not a string");
@@ -31,5 +36,44 @@ public class MyProgram {
         }
         while(!in.nextLine().equals("0"));
         in.close();
+        */
+
+
+        //
+        Scanner in2 = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter the number > 0:");
+
+            int value = in2.nextInt();
+            int flowerCount = 1;
+
+            if(value <= 0){
+                System.out.println("Value is incorrect. Stop.");
+            }
+            else if(value == 1){
+                System.out.println("Flower count: 1");
+
+            }
+            else{
+                flowerCount = value + (value - 1); // 0 +
+                System.out.println("Flower count: " + flowerCount);
+
+            }
+
+            Robot r = new Robot();
+            r.keyPress(KeyEvent.VK_1);
+            r.keyRelease(KeyEvent.VK_1);
+            r.keyPress(KeyEvent.VK_ENTER);
+            r.keyRelease(KeyEvent.VK_ENTER);
+
+            System.out.println("Enter -1 for exit");
+
+
+        }
+        while(in2.nextInt() != -1);
+
+        in2.close();
+
     }
 }
